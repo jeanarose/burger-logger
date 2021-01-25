6cce8e0c93fd0c703ef3d15ca19bf47acc306c7f
Jeana Rose <jr.mathis608@gmail.com>
2021-01-25 16:36:43 -0500

    Added data to schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Classic Burger');
+INSERT INTO burgers (name) VALUES ('Mushroom and Swiss Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Loaded Mac and Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Pulled Pork Burger', true);
+INSERT INTO burgers (name) VALUES ('Black Bean Burger');
